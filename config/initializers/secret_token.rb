bf39c5042cda917de131d6bda5386a2babfbf68a
zh <overeyes@gmail.com>
2014-03-30 16:41:07 +0400

    Initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TravisTest::Application.config.secret_token = '9845c4e610e166aae10d0ac605c250849e38d87c7eba4d5da93e1cdec852d6074496b1e3ab313787d1892a3ff3447496a661e7780f16d8c6d55079c06461c2cd'
